6b90768aa6564e7154257507519d25bc8c63a6ec
Mohaymen Omar <mohaymenomar13@gmail.com>
2024-09-23 08:16:03 +0800

    Added Quiz Mistakes Review

--- src/StartQuiz.js
+++ src/StartQuiz.js
@@ -13,6 +13,7 @@ function StartQuiz(props) {
     };
 
     const [questions, setQuestions] = useState([]);
+    const [mistakenQuestions, setMistakenQuestions] = useState([]); 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState('');
     const [answerInput, setAnswerInput] = useState('');
@@ -43,11 +44,17 @@ function StartQuiz(props) {
         return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     };
 
-    const handleSubmit = () => {
+    console.log(mistakenQuestions)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         if (answerInput.toLowerCase() === currentQuestion.answer.toLowerCase()) {
             setCorrectAnswers(correctAnswers + 1);
         } else {
+            setMistakenQuestions([...mistakenQuestions, {question: currentQuestion.question, userAnswer: answerInput, answer: currentQuestion.answer}]);
             setMistakes(mistakes + 1);
+            //console.log(currentQuestion.question + " " + currentQuestion.answer + " " + answerInput);
         }
 
         if (currentQuestionIndex < questions.length - 1) {
@@ -62,49 +69,73 @@ function StartQuiz(props) {
     };
 
     return (
-        <div className="sq-main-content">
-            <h1 style={{fontSize: "2rem", fontWeight:"bold", marginBottom:"32px"}}>Test7</h1>
-            <div className="sq-quiz-container">
-                <div className="sq-question-card">
-                    <div className="sq-question">
-                        <p id="sq-question-text">{currentQuestion.question}</p>
-                    </div> 
-                    <input 
-                        type="text" 
-                        placeholder="Your answer..." 
-                        className="sq-input-field" 
-                        id="answerInput" 
-                        value={answerInput} 
-                        onChange={(e) => setAnswerInput(e.target.value)}
-                    />
-                    <button 
-                        id="submitBtn" 
-                        className="sq-submit-btn" 
-                        onClick={handleSubmit} 
-                        disabled={quizFinished}
-                    >
-                        Submit
-                    </button>
-                    <div className="sq-progress-bar-container">
-                        <div 
-                            className="sq-progress-bar" 
-                            id="progressBar" 
-                            style={{width: `${((currentQuestionIndex+1) / questions.length) * 100}%`}}
-                        ></div>
-                    </div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 16 }}>
-                        <p style={{ fontSize: "0.875rem" }}>Question <span id="currentQuestionIndex">{currentQuestionIndex + 1}</span></p>
-                        <p style={{ fontSize: "0.875rem" }}>Time Consumed: <span id="timeConsumed" style={{fontWeight:"bold"}}>{formatTime(timeConsumed)}</span></p>
-                    </div>
-                    <div style={{ marginTop: -15, display: 'flex', justifyContent: 'space-between' }}>
-                        <p style={{ fontSize: "0.875rem" }}>Correct: <span id="correctAnswers">{correctAnswers}</span></p>
-                        <p style={{ fontSize: "0.875rem" }}>Mistakes: <span id="mistakes">{mistakes}</span></p>
-                    </div>
-                    <button id="saveBtn" className="sq-save-btn">Exit</button>
+        <div>
+            {quizFinished ? (
+                <div className="sq-mistaken-questions-container">
+                    <h2>Your Mistaken Questions</h2>
+                    <p>Score: {questions.length-mistakes}/{questions.length}</p>
+                    <p>Percentage: {((questions.length-mistakes)/questions.length)*100}%</p>
+                    {mistakenQuestions.length > 0 ? (
+                        mistakenQuestions.map((mistake, index) => (
+                            <div key={index} className="mistaken-question-card">
+                                <p><strong>Question:</strong> {mistake.question}</p>
+                                <p><strong>Your Answer:</strong> {mistake.userAnswer}</p>
+                                <p><strong>Correct Answer:</strong> {mistake.answer}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No mistakes! Well done!</p>
+                    )}
                 </div>
-            </div>
+                ) 
+                : 
+                (
+                <div className="sq-main-content">
+                    <h1 style={{fontSize: "2rem", fontWeight:"bold", marginBottom:"32px"}}>Test7</h1>
+                    <div className="sq-quiz-container">
+                        <div className="sq-question-card">
+                            <form onSubmit={handleSubmit}>
+                                <div className="sq-question">
+                                    <p id="sq-question-text">{currentQuestion.question}</p>
+                                </div> 
+                                <input 
+                                    type="text" 
+                                    placeholder="Your answer..." 
+                                    className="sq-input-field" 
+                                    id="answerInput" 
+                                    value={answerInput} 
+                                    onChange={(e) => setAnswerInput(e.target.value)}
+                                />
+                                <button 
+                                    id="submitBtn" 
+                                    className="sq-submit-btn" 
+                                    disabled={quizFinished}
+                                >
+                                Submit
+                                </button>
+                            </form>
+                            <div className="sq-progress-bar-container">
+                                <div 
+                                    className="sq-progress-bar" 
+                                    id="progressBar" 
+                                    style={{width: `${((currentQuestionIndex+1) / questions.length) * 100}%`}}
+                                ></div>
+                            </div>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: 16 }}>
+                                <p style={{ fontSize: "0.875rem" }}>Question <span id="currentQuestionIndex">{currentQuestionIndex + 1}</span></p>
+                                <p style={{ fontSize: "0.875rem" }}>Time Consumed: <span id="timeConsumed" style={{fontWeight:"bold"}}>{formatTime(timeConsumed)}</span></p>
+                            </div>
+                            <div style={{ marginTop: -15, display: 'flex', justifyContent: 'space-between' }}>
+                                <p style={{ fontSize: "0.875rem" }}>Correct: <span id="correctAnswers">{correctAnswers}</span></p>
+                                <p style={{ fontSize: "0.875rem" }}>Mistakes: <span id="mistakes">{mistakes}</span></p>
+                            </div>
+                            <button id="saveBtn" className="sq-save-btn">Exit</button>
+                        </div>
+                    </div>
+                </div>
+                )}
         </div>
-    );
+    )
 }
 
 export default StartQuiz;
