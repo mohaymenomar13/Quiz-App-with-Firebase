20f8320c32d31cc4aa1826a2a375f2d121b71a21
Mohaymen Omar <mohaymenomar13@gmail.com>
2024-10-01 11:02:23 +0800

    made a changes in startquiz.js

--- src/StartQuiz.js
+++ src/StartQuiz.js
@@ -80,7 +80,7 @@ function StartQuiz(props) {
                     {mistakenQuestions.length > 0 ? (
                         mistakenQuestions.map((mistake, index) => (
                             <div key={index} className="mistaken-question-card">
-                                <p><strong>Question:</strong> {mistake.question}</p>
+                                <p dangerouslySetInnerHTML={{ __html: mistake.question }}><strong>Question:</strong></p>
                                 <p><strong>Your Answer:</strong> {mistake.userAnswer}</p>
                                 <p><strong>Correct Answer:</strong> {mistake.answer}</p>
                             </div>
